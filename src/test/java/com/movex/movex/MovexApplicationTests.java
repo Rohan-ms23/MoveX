5a50f5edccc2fad0149478011463c12242010cf1
Rohan_ro23 <rohanms0405@gmail.com>
2026-07-02 12:55:18 +0530

    Initialize MoveX project setup

---
+++ src/test/java/com/movex/movex/MovexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movex.movex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
